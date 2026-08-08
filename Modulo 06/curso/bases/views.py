f50dea3e31245fc98980e9c97e28adf6d3b0aa94
Marcos C. Vargas M. <esocram@gmail.com>
2026-08-07 19:08:24 -0600

    M06: avances

--- Modulo 06/curso/bases/views.py
+++ Modulo 06/curso/bases/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pdb
 
 
 # Create your views here.
 def vista_funcion(request):
-    return render(request, 'bases/base.html')
+    if request.method == 'GET':
+        return render(request, 'bases/index.html', {'tarjetas':range(1, 8)})
+    elif request.method == 'POST':
+        return render(request, 'bases/index.html', {'tarjetas':range(1, 8), 'datos':request.POST})
 

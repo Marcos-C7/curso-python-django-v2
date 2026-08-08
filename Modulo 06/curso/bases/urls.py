f7ac939871004512915bbfd6f5675439dc0ce304
Marcos C. Vargas M. <esocram@gmail.com>
2026-08-07 20:46:27 -0600

    M06: avances

--- Modulo 06/curso/bases/urls.py
+++ Modulo 06/curso/bases/urls.py
@@ -6,5 +6,8 @@ app_name = 'bases'
 urlpatterns = [
     path('vista_funcion/', views.vista_funcion, name='vista_funcion'),
     path('vista_clase/', views.VistaClase.as_view(), name='vista_clase'),
+    path('vista_plantilla/', views.MiPlantilla.as_view(), name='vista_plantilla'),
+    path('vista_producto/', views.VistaProducto.as_view(), name='vista_producto'),
+    path('vista_plantilla_django/', views.VistaPlantillaDjango.as_view(), name='vista_plantilla_django'),
 ]
 
--- Modulo 06/curso/bases/views.py
+++ Modulo 06/curso/bases/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
+from django.views.generic import TemplateView
 import pdb
 
 
@@ -18,4 +19,47 @@ class VistaClase(View):
 
     def post(self, request):
         return render(request, 'bases/index.html', {'tarjetas':range(1, 8), 'datos':request.POST})
+
+
+class VistaPlantilla(View):
+    plantilla = None
+    contexto_extra = None
+
+    def get(self, request):
+        return render(request, self.plantilla, self.contexto_extra)
+
+    def post(self, request):
+        contexto = {'datos':request.POST}
+        contexto.update(self.contexto_extra)
+
+        return render(request, self.plantilla, contexto)
+
+
+class MiPlantilla(VistaPlantilla):
+    plantilla = 'bases/index.html'
+    contexto_extra = {'tarjetas':range(1, 8)}
+
+
+class VistaProducto(VistaPlantilla):
+    plantilla = 'bases/index_2.html'
+    contexto_extra = {'producto': 'Agua', 'precio': 10, 'cantidad': 2}
+
+
+class VistaPlantillaDjango(TemplateView):
+    template_name = 'bases/index.html'
+    extra_context = {'tarjetas':range(1, 11)}
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+
+        if self.request.method == 'POST':
+            contexto['datos'] = self.request.POST
+
+        return contexto
+    
+    def post(self, request):
+        contexto = self.get_context_data()
+
+        return render(request, self.template_name, contexto)
+
 
